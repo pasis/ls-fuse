dc78492c7fc4684cc6f83ef76d71d2514aa52153
Dmitry Podgorny <pasis.ua@gmail.com>
2013-03-17 14:58:48 +0200

    fixed regex lsreg_str

--- src/main.c
+++ src/main.c
@@ -52,7 +52,7 @@ static regex_t lsreg;
 static char lsreg_str[] =
 	"^([-bcdlpsS])([-rwxs]{9,9})[ \t]+[0-9]+[ \t]+([0-9A-Za-z]+)[ \t]+"
 	"([0-9A-Za-z]+)[ \t]+([0-9]+|[0-9]+,[ \t]+[0-9]+)[ \t]+([^ \t]+)[ \t]+"
-	"([1-3]?[0-9][ \t]+[1-2]?[0-9]:[0-5][0-9]|[1-3]?[0-9][ \t]+[0-9]{4,4})"
+	"([1-3]?[0-9][ \t]+[0-2]?[0-9]:[0-5][0-9]|[1-3]?[0-9][ \t]+[0-9]{4,4})"
 	"[ \t]+(.+)$";
 static handler_t lsreg_tbl[MATCH_NUM] = {NULL, &node_set_type, &node_set_mode,
 	NULL, NULL, NULL, NULL, NULL, &node_set_name,};
